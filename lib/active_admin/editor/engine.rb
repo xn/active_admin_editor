5282595b58804f81bde74183eb3167e5a8abf53e
Christian Trosclair <xn@users.noreply.github.com>
2017-11-06 11:18:11 -0600

    remove deprecated registers

--- lib/active_admin/editor/engine.rb
+++ lib/active_admin/editor/engine.rb
@@ -12,15 +12,6 @@ module ActiveAdmin
           'active_admin/editor.js',
           'active_admin/editor.css'
         ] + ActiveAdmin::Editor.configuration.stylesheets
-
-        ActiveAdmin.application.tap do |config|
-          config.register_javascript 'active_admin/editor.js'
-          config.register_stylesheet 'active_admin/editor.css'
-
-          ActiveAdmin::Editor.configuration.stylesheets.each do |stylesheet|
-            config.register_stylesheet stylesheet
-          end
-        end
       end
     end
   end
